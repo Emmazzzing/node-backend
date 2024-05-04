9200b4206d7d2d1ab665a2356513434eb236a8fe
Emmazzzing <liu.jing6@northeastern.edu>
2024-05-04 14:24:11 -0700

    rewrite user signup use user model

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,6 +1,7 @@
 const uuid = require("uuid");
 const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
+const User = require("../models/user");
 
 const DUMMY_USERS = [
   {
@@ -14,28 +15,48 @@ const DUMMY_USERS = [
 const getUsers = (req, res, next) => {
   res.json({ users: DUMMY_USERS });
 };
-const signup = (req, res, next) => {
+
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError("Invalid input passed, please check your data", 422);
+    return next(
+      new HttpError("Invalid input passed, please check your data", 422)
+    );
   }
-  const { name, email, password } = req.body;
-  const hasUser = DUMMY_USERS.find((u) => {
-    u.email === email;
-  });
-  if (hasUser) {
-    const error = new HttpError("User already exits", 402);
+  const { name, email, password, places } = req.body;
+  let hasUser;
+  try {
+    hasUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      "can not sign up now, please try again later",
+      500
+    );
     return next(error);
   }
-  const newUser = {
-    id: uuid.v4,
+
+  if (hasUser) {
+    const error = new HttpError("User already exits", 422);
+    return next(error);
+  }
+  const newUser = new User({
     name,
     email,
+    image: "https://live.staticflickr.com/7631/26849088292_36fc52ee90_b.jpg",
     password,
-  };
-  DUMMY_USERS.push(newUser);
-  res.status(201).json({ users: newUser });
+    places,
+  });
+
+  try {
+    await newUser.save();
+  } catch (err) {
+    const error = new HttpError("Creating user failed, please try again", 500);
+    return next(error);
+  }
+
+  res.status(201).json({ users: newUser.toObject({ getters: true }) });
 };
+
 const login = (req, res, next) => {
   const { email, password } = req.body;
   const user = DUMMY_USERS.find((u) => u.email === email);
