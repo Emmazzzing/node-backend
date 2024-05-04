8e1851f9f1440ef8725c0b5adad82fd54d330cb2
Emmazzzing <liu.jing6@northeastern.edu>
2024-05-03 22:52:07 -0700

    check user input validation use validate api

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const { check } = require("express-validator");
 const placeControllers = require("../controllers/places-controllers");
 const router = express.Router();
 
 router.get("/:pid", placeControllers.getPlaceById);
 router.get("/user/:uid", placeControllers.getPlacesByUserId);
-router.post("/", placeControllers.createPlace);
-router.patch("/:pid", placeControllers.updatePlace);
+router.post(
+  "/",
+  [
+    check("title").notEmpty(),
+    check("description").isLength({ min: 5 }),
+    check("address").notEmpty(),
+  ],
+  placeControllers.createPlace
+);
+
+router.patch(
+  "/:pid",
+  [check("title").notEmpty(), check("description").isLength({ min: 5 })],
+  placeControllers.updatePlace
+);
 router.delete("/:pid", placeControllers.deletePlace);
 module.exports = router;
