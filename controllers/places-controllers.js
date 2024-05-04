b9b9323af00996840d7c8138d18eaf07a1d4f0fc
Emmazzzing <liu.jing6@northeastern.edu>
2024-05-03 21:19:07 -0700

    adding place patch routes

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -57,6 +57,30 @@ const createPlace = (req, res, next) => {
   res.status(200).json({ place: createdPlace });
 };
 
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
+
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({ place: updatedPlace });
+};
+
+const deletePlace = (req, res, next) => {
+  const targetPlaceId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === targetPlaceId;
+  });
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
